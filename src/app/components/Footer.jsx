379fe1f01abfca0f16c300056f03eece177340dd
irfanulhaqueiffat <irfanifatcbx@gmail.com>
2025-11-16 23:59:10 +0600

    navbar

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    </div>
+  )
+}
+
+export default footer
---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const page = () => {
+  return (
+    <>
+  
+
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default page
